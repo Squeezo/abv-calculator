4f68a5b643fbf0ad720540b799b50cde7b5da3b4
Squeezo <squeezemccoy@gmail.com>
2019-10-14 11:14:46 -0700

    unit tests on GetAbv function

--- src/CalcForm.test.tsx
+++ src/CalcForm.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import CalcForm from './CalcForm';
+import CalcForm  from './CalcForm';
+import { GetAbv } from './CalcForm';
 
 describe("CalcForm Component", ()=> {
   it('CalcForm displays the default value', () => {
@@ -23,4 +24,28 @@ describe("CalcForm Component", ()=> {
   const calcForm = shallow(<CalcForm />);  
   expect(calcForm).toMatchSnapshot();
   })
+});
+
+describe('GetAbv function', () => {
+  it('returns the correct value for valid inputs', () => {
+    expect(GetAbv({ og: 1.056, fg: 1.015 })).toEqual(5.38);
+  });
+
+  it('gracefully handles nulls', () => {
+    expect(GetAbv({ fg: 1.015 })).toEqual(0);
+    expect(GetAbv({ og: 1.058 })).toEqual(0);
+  });
+
+  it('block values under 1.000',()=>{
+    expect(GetAbv({ og: 1.056, fg: 0.9999 })).toEqual(0);
+    expect(GetAbv({ og: 0.9999, fg: 1.008 })).toEqual(0);
+    expect(GetAbv({ og: 0.9999, fg: 0.9999 })).toEqual(0);
+  });
+
+  it('block values over 1.999',()=>{
+    expect(GetAbv({ og: 1.056, fg: 2.000 })).toEqual(0);
+    expect(GetAbv({ og: 2.000, fg: 1.008 })).toEqual(0);
+    expect(GetAbv({ og: 2.000, fg: 2.000 })).toEqual(0);
+  });
+
 });
--- src/CalcForm.tsx
+++ src/CalcForm.tsx
@@ -14,7 +14,7 @@ export default class CalcForm extends React.Component<{}, State> {
   handleChange = (e: React.FormEvent<HTMLInputElement>) => {
       let name= e.currentTarget.name;
       let value = e.currentTarget.value;
-      this.setState(Object.assign({}, this.state, { [name]: value } ));
+      this.setState(Object.assign({}, this.state, { [name]: Number.parseFloat(value) } ));
   };
 
   render () {
@@ -24,7 +24,8 @@ export default class CalcForm extends React.Component<{}, State> {
         <div className='form'>
           <div className='fields'>
             <div>
-                <label htmlFor='og'>OG:</label><input name='og' id='og' type='number' step={0.001} value={this.state.og} onChange={this.handleChange} />
+                <label htmlFor='og'>OG:</label>
+                <input name='og' id='og' type='number' step={0.001} min={1.00} max={1.999} value={this.state.og} onChange={this.handleChange} />
             </div>
             <div>
                 <label htmlFor='fg'>FG: </label><input name='fg' id='fg' type='number' step={0.001} value={this.state.fg} onChange={this.handleChange} />
@@ -33,7 +34,7 @@ export default class CalcForm extends React.Component<{}, State> {
           </div>
           <div className='abv-display'>
             <span>ABV: </span>
-            <span className='abv'>{calc(this.state.og, this.state.fg)}%</span>
+            <span className='abv'>{GetAbv({ og: this.state.og, fg: this.state.fg })}%</span>
           </div>
           
         </div>
@@ -44,6 +45,10 @@ export default class CalcForm extends React.Component<{}, State> {
   }
 }
 
-function calc(og: number, fg: number) : number {
+export const GetAbv = ({ og, fg }: { og: number; fg: number; }) : any => {
+  if (!og || !fg) return 0;
+  if((og && typeof(og) === 'string') || (fg && typeof(fg) === 'string')) return 0;
+  if((og && og < 1) || (fg && fg < 1)) return 0;
+  if((og && og > 1.999) || (fg && fg > 1.999)) return 0;
   return Math.floor(((og - fg) * 131.25) * 100) / 100;
 }
